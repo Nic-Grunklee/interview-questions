0fa585ff5e591b94c32a9bcf5761d6767684ee20
Nic <ngrunks@gmail.com>
2019-08-15 18:55:05 -0500

    reverse string

---
+++ reverse-string.js
@@ -0,0 +1,13 @@
+const string = "abrkaabcdefghijjxxx";
+
+reverseString(string);
+
+function reverseString(val) {
+    let reverseString = '';
+
+    for(let i = val.length - 1; i >=0; i--) {
+        reverseString += val.charAt(i);
+    }
+
+    console.log(reverseString);
+}
